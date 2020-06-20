9dcd880ae03acffd60d03c0c412b60efcd802677
Dagantt <53485758+Dagantt@users.noreply.github.com>
2020-06-20 19:22:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+
+import discord
+from discord import Member, VoiceChannel
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        members = '\n - '.join([member.name for member in guild.members])
+        print(
+            f'{client.user.name} is connected to the following guild:\n'
+            f'- {guild.name}(id: {guild.id})'
+        )
+
+bot = commands.Bot(command_prefix='µ')
+
+@bot.command(name='ping', help='Responds with Pong')
+async def ping(ctx):
+    response = 'Pong'
+    await ctx.send(response)
+
+@bot.command(name='play', help='temp : play yt video passed in argument', pass_context = True)
+async def play(ctx, url):
+    author = ctx.message.author
+    voice_channel = author.voice.channel
+    vc = await VoiceChannel.connect(voice_channel)
+
+    player = await vc.create_ytdl_player(url)
+    player.start()
+
+    response = 'Playing ' + url + " in " + voice_channel;
+    await ctx.send(response)
+
+bot.run(TOKEN)
+
+
+client.run(TOKEN)
